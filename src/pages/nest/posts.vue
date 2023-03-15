ccf15c8fbbffe81101ee32278a5450614eccb578
Gary <gary.cai@hlwtech.com.tw>
2023-03-15 11:52:09 +0800

    修正巢狀路由連結

--- src/pages/nest/posts.vue
+++ src/pages/nest/posts.vue
@@ -3,8 +3,8 @@
     <div>
       <h1>這裡是 Posts</h1>
       <div>
-        <p><NuxtLink to="../posts/post1">前往 Post 1</NuxtLink></p>
-        <p><NuxtLink to="../posts/post2">前往 Post 2</NuxtLink></p>
+        <p><NuxtLink to="/nest/posts/post1">前往 Post 1</NuxtLink></p>
+        <p><NuxtLink to="/nest/posts/post2">前往 Post 2</NuxtLink></p>
       </div>
     </div>
     <div>
